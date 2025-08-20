b1a4cc726c649efc00254ab3767eeba67c0fc94a
lilushhh <lilyelimelech@gmail.com>
2025-08-20 10:15:00 +0300

    feat: changed the update user request to extend from create user request

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -4,9 +4,4 @@ export interface CreateUserRequest {
     last_name: string;
     unit: string;
 }
-export interface UpdateUserRequest {
-    personal_id: string;
-    first_name: string;
-    last_name: string;
-    unit: string;
-}
+export interface UpdateUserRequest extends CreateUserRequest {}
