1860d206d932c69bc23ca904fe6844f91004f28a
lilushhh <lilyelimelech@gmail.com>
2025-08-20 10:11:17 +0300

    feat: added user interface

---
+++ src/interfaces/UserInterface.ts
@@ -0,0 +1,12 @@
+export interface CreateUserRequest {
+    personal_id: string;
+    first_name: string;
+    last_name: string;
+    unit: string;
+}
+export interface UpdateUserRequest {
+    personal_id: string;
+    first_name: string;
+    last_name: string;
+    unit: string;
+}
