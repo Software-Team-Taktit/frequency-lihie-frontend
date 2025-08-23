28a9f5b7e22c1fc56786edb9ece06c7232dd99a1
lilushhh <lilyelimelech@gmail.com>
2025-08-23 14:39:16 +0300

    feat: added response interface and a log in request interface

--- src/interfaces/AdminInterface.ts
+++ src/interfaces/AdminInterface.ts
@@ -1,5 +1,18 @@
-import type { CreateUserRequest } from "./UserInterface";
+import type { CreateUserRequest, UserType} from "./UserInterface";
 
 export interface CreateAdminRequest extends CreateUserRequest {}
 
 export interface UpdateAdminRequest extends CreateUserRequest {}
+
+export interface Admin {
+    id: string;
+    readonly type: UserType;
+    personal_id: string;
+    first_name: string;
+    last_name: string;
+    unit: string;
+}
+
+export interface AdminLogInRequest {
+    personal_id: string;
+}
