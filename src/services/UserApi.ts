2ca11449db39663aad8b77161bd9d13c374888fe
lilushhh <lilyelimelech@gmail.com>
2025-08-23 14:08:21 +0300

    feat: added request function

--- src/services/UserApi.ts
+++ src/services/UserApi.ts
@@ -11,3 +11,23 @@ export class HttpError extends Error {
         this.data = data;
     }
 }
+
+async function request<T>(path: string, init: RequestInit = {}): Promise<T> {
+    const res = await fetch(`${BASE_URL}${path}`, {
+        ...init,
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            ...(init.headers || {})
+        }
+    });
+    const ct = res.headers.get("content-type") || "";
+    const isJson = ct.includes("application/json");
+    const data = isJson ? await res.json().catch(()=> null) : null;
+
+    if(!res.ok){
+        const msg = (data?.detail && String((data as any).detail)) || res.statusText || "Request failed";
+        throw new HttpError(res.status, msg, data);
+    }
+    return data as T;
+}
