fb1a931132fd3f44972d1cdee59b623c898844d0
lilushhh <lilyelimelech@gmail.com>
2025-08-23 13:44:14 +0300

    feat: added response interface and a log in request interface

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -1,3 +1,5 @@
+export type UserType = "user" | "admin" | "mission" | "platform";
+
 export interface CreateUserRequest {
     personal_id: string;
     first_name: string;
@@ -5,3 +7,16 @@ export interface CreateUserRequest {
     unit: string;
 }
 export interface UpdateUserRequest extends CreateUserRequest {}
+
+export interface User {
+    id: string;
+    readonly type: UserType;
+    personal_id: string;
+    first_name: string;
+    last_name: string;
+    unit: string;
+}
+
+export interface UserLogInRequest {
+    personal_id: string;
+}
