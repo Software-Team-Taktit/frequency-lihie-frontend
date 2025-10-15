8c3cea174856447d9dc52d5facb1489f7d876919
lilushhh <lilyelimelech@gmail.com>
2025-10-15 13:47:27 +0300

    fix: fix the scroll-y in the mission gallery

--- src/components/mission/MissionGallery.tsx
+++ src/components/mission/MissionGallery.tsx
@@ -26,20 +26,22 @@ function MissionGallery() {
     }
 
     return(
-        <div className="bg-blue-100 rounded-xl p-8 md:p-10 w-[1800px] h-[750px] mx-auto shadow-md flex flex-col gap-8">
+        <div className="bg-blue-100 rounded-xl p-8 md:p-10 w-[1800px] h-[750px] mx-auto shadow-md flex flex-col gap-8" dir="rtl">
             <div className="flex items-center justify-between">
                 <Button className="flex items-center gap-2 bg-white text-blue-600 hover:bg-blue-700 rounded-full px-4 py-2"
                 onClick={()=> navigate("/missionForm")}>
                     <Plus className="w-4 h-4"/>
                 </Button>
                 <h1 className="suez-one-regular text-6xl text-center text-blue-700">משימות</h1>
-                <div className="grid grid-cols-3 items-center"></div>
+                <div className="w-12"></div>
             </div>
-            <section className="grid gap-6 grid-cols-1 w-[700px] sm:grid-cols-2 lg:grid-cols-3">
-                {items?.map((m)=> (
-                    <MissionCard key={m.id} m={m} onEdit={setEditing} onDelete={handleDelete}/>
-                ))}
-            </section>
+            <div className="flex-1 overflow-y-auto pr-2">
+                <section className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 auto-rows-max">
+                    {items?.map((m) => (
+                        <MissionCard key={m.id} m={m} onEdit={setEditing} onDelete={handleDelete}/> 
+                    ))}
+                </section>
+            </div>
             <Dialog open={!!editing} onOpenChange={(v) => !v && setEditing(null)}>
                 <DialogContent className="sm:max-w-[600px] rounded-2xl bg-blue-100 border border-black">
                     <DialogHeader>
