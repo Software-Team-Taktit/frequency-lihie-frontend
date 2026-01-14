4b103fc2742b14a10091ba8786ef75d1db5769d7
lilushhh <lilyelimelech@gmail.com>
2026-01-14 19:50:40 +0200

    feat: added unit label and administaritor label

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -45,9 +45,10 @@ function NavBar() {
                                 <Button className="huninn-regular hover:text-blue-600 font-bold text-2xl px-4 py-2 ml-4 text-gray-800 m" onClick={handleLogout}>התנתקות</Button>
                                 <div className="text-right leading-tight text-2xl">
                                     <div className="font-semibold text-gray-800 dark:text-gray-100 huninn-regular">
-                                        {user.first_name} {user.last_name} 
+                                        {user.first_name} {user.last_name} - {user.unit}
                                     </div>
-                                    <div className="text-xs text-gray-500">{user.personal_id}</div>
+                                    <div className="text-xs text-gray-500">{user.type === "admin" ? 
+                                    `${user.personal_id} - administrator` : user.personal_id}</div>
                                 </div>
                             </>
                         )
